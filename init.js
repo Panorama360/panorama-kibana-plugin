738ae6c78b46d7f0ef7a171d574c7663c502fd78
Rafael Ferreira da Silva <rafsilva@isi.edu>
2018-03-26 15:23:02 -0700

    enabling workflow rendering via cytoscape

--- init.js
+++ init.js
@@ -7,9 +7,13 @@ export default function (server) {
             callWithRequest(request, 'search', {
                 index: 'panorama',
                 body: {
-                    'query': {
-                        'match': {
-                            "event": 'stampede.job.info'
+                    query: {
+                        bool: {
+                            must: {
+                                term: {
+                                    'event': 'stampede.job.info'
+                                }
+                            }
                         }
                     }
                 }
@@ -20,6 +24,87 @@ export default function (server) {
                         data.wf_ids.push(response.hits.hits[i]._source['xwf.id']);
                     }
                 }
+                reply(data);
+            })
+        }
+    });
+
+    server.route({
+        path: '/api/panorama/get/wf/{wf_id}',
+        method: 'GET',
+        handler(request, reply) {
+            const {callWithRequest} = server.plugins.elasticsearch.getCluster('data');
+            callWithRequest(request, 'search', {
+                index: 'panorama',
+                size: 50,
+                body: {
+                    query: {
+                        bool: {
+                            must: {
+                                match: {'xwf.id': request.params.wf_id}
+                            },
+                            should: [
+                                {term: {'event': 'stampede.job.edge'}},
+                                {term: {'event': 'stampede.job.info'}}
+                            ],
+                            minimum_should_match: 1,
+                            boost: 1.0
+                        }
+                    }
+                }
+            }).then(response => {
+                let data = {'wf_id': request.params.wf_id, 'jobs': []};
+                let res = response.hits.hits;
+
+                for (let i = 0, len = res.length; i < len; i++) {
+
+                    if (res[i]._source['event'] === 'stampede.job.info') {
+                        let job_id = res[i]._source['job.id'];
+                        let job = null;
+                        for (let j = 0, len = data.jobs.length; j < len; j++) {
+                            if (data.jobs[j].job_id === job_id) {
+                                job = data.jobs[j];
+                                break;
+                            }
+                        }
+                        if (!job) {
+                            job = {'job_id': job_id, parents: [], children: []};
+                            data.jobs.push(job);
+                        }
+                        job.type = res[i]._source['type_desc'];
+
+                    } else if (res[i]._source['event'] === 'stampede.job.edge') {
+                        let parent = res[i]._source['parent.job.id'];
+                        let child = res[i]._source['child.job.id'];
+                        let parentNotSet = true;
+                        let childNotSet = true;
+
+                        for (let j = 0, len = data.jobs.length; j < len; j++) {
+                            if (data.jobs[j].job_id === parent) {
+                                data.jobs[j].children.push(child);
+                                parentNotSet = false;
+                            } else if (data.jobs[j].job_id === child) {
+                                data.jobs[j].parents.push(parent);
+                                childNotSet = false;
+                            }
+                        }
+                        if (parentNotSet) {
+                            data.jobs.push({
+                                'job_id': parent,
+                                parents: [],
+                                children: [child]
+                            });
+                        }
+                        if (childNotSet) {
+                            data.jobs.push({
+                                'job_id': child,
+                                parents: [parent],
+                                children: []
+                            });
+                        }
+                    }
+                }
+                console.log(data);
                 reply(data);
             })
         }
--- public/controllers/workflow_controller.js
+++ public/controllers/workflow_controller.js
@@ -1,6 +1,107 @@
+import cytoscape from 'cytoscape';
+
 const app = require('ui/modules').get('apps/panorama', []);
 
-app.controller('workflow', function ($scope, $http, kbnUrl, $routeParams, Private, timefilter) {
+const node_colors = {
+    'compute': '#668d3c',
+    'cleanup': '#007996',
+    'create-dir': '#c3b7ac',
+    'stage-out-tx': '#0097ac',
+    'stage-in-tx': '#0097ac',
+    'registration': '#816c5b'
+};
 
-    console.log($routeParams.id);
+app.factory('workflowGraph', ['$q', function ($q) {
+
+    let cy;
+
+    let workflowGraph = function (workflow) {
+        let deferred = $q.defer();
+
+        let elements = [];
+        for (let i = 0; i < workflow.jobs.length; i++) {
+            let job = workflow.jobs[i];
+
+            elements.push({
+                group: 'nodes',
+                data: {
+                    id: job.job_id,
+                    name: job.job_id,
+                    type: job.type,
+                    faveBgColor: node_colors[job.type]
+                }
+            });
+            for (let j = 0; j < job.parents.length; j++) {
+                let parent = job.parents[j];
+                elements.push({
+                    group: 'edges',
+                    data: {
+                        id: 'edge_' + parent + '_' + job.job_id,
+                        source: parent,
+                        target: job.job_id
+                    }
+                })
+            }
+        }
+
+        $(function () { // on dom ready
+
+            cy = cytoscape({
+                container: $('#cy')[0],
+
+                style: cytoscape.stylesheet()
+                    .selector('node')
+                    .css({
+                        'content': 'data(name)',
+                        'background-color': 'data(faveBgColor)',
+                        'text-valign': 'center',
+                        'color': 'white',
+                        'text-wrap': 'wrap',
+                        'width': '200',
+                        'height': '70',
+                        'shape': 'roundrectangle',
+
+                    })
+                    .selector('edge')
+                    .css({
+                        'target-arrow-shape': 'triangle',
+                        'curve-style': 'bezier'
+                    }),
+
+                layout: {
+                    name: 'breadthfirst',
+                    directed: true,
+                    padding: 2,
+                    spacingFactor: 0.75
+                },
+
+                elements: elements,
+
+                ready: function () {
+                    deferred.resolve(this);
+
+                }
+            });
+
+        }); // on dom ready
+
+        return deferred.promise;
+    };
+
+    workflowGraph.listeners = {};
+
+    return workflowGraph;
+}]);
+
+app.controller('workflow', function ($scope, $http, kbnUrl, $routeParams, workflowGraph, Private, timefilter) {
+
+    $http.get('../api/panorama/get/wf/' + $routeParams.id).then((response) => {
+        $scope.workflow = response.data;
+
+        let cy;
+        workflowGraph($scope.workflow).then(function (workflowCy) {
+            cy = workflowCy;
+            $scope.cyLoaded = true;
+        });
+    });
 });
